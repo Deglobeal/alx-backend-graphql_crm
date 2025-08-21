b4b9e4f62cdfb1beae68f824303d94f9df8fcaa9
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-08-21 10:46:04 +0100

    file fixed

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,9 @@
 import graphene
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
+    hello = graphene.String()
+    
+    def resolve_hello(self, info):
+        return "Hello, GraphQL!"
 
 schema = graphene.Schema(query=Query)

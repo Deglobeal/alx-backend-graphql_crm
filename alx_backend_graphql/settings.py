a7b98d1bc334eac96826a86a445f8cb97359b02a
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-08-21 10:26:18 +0100

    file fixed

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crm',                 # <-- our new app
+    'graphene_django',     # <-- graphene
+    'django_filters',      # <-- django-filter
 ]
 
 MIDDLEWARE = [
